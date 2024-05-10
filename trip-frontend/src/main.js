aaaf849fc796b4f098459c873456d678df89ad05
heesu <s007kk@ajou.ac.kr>
2024-05-10 17:54:15 +0900

    feat: modify navBar design

--- trip-frontend/src/main.js
+++ trip-frontend/src/main.js
@@ -7,10 +7,14 @@ import store from "./stores";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 const app = createApp(App);
+library.add(faUserSecret);
+app.component("font-awesome-icon", FontAwesomeIcon);
 
-// app.use(createPinia());
 app.use(router);
 
 app.use(store).mount("#app");

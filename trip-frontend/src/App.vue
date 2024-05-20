627b0e9bbca4bae19e765faf1d9ec3c317894964
kangwoo <rinch12332@gmail.com>
2024-05-20 15:37:22 +0900

    05-20 15:37 aibot

--- trip-frontend/src/App.vue
+++ trip-frontend/src/App.vue
@@ -1,10 +1,29 @@
 <script setup>
 import { RouterView } from "vue-router";
 import TheHeadingNavbar from "@/components/common/TheHeadingNavbar.vue";
+import { onMounted } from "vue";
+const { VITE_SEND_BIRD_APP_ID } = import.meta.env;
+
+onMounted(() => {
+  (function (w, d, s, ...args) {
+    var div = d.createElement("div");
+    div.id = "aichatbot";
+    d.body.appendChild(div);
+    w.chatbotConfig = args;
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s);
+    j.defer = true;
+    j.type = "module";
+    j.src = "https://aichatbot.sendbird.com/index.js";
+    f.parentNode.insertBefore(j, f);
+  })(window, document, "script", VITE_SEND_BIRD_APP_ID, "onboarding_bot", {
+    apiHost: "https://api-cf-ap-2.sendbird.com",
+  });
+});
 </script>
 
 <template>
-  <div>
+  <div id="chatbot-container">
     <TheHeadingNavbar />
     <router-view />
   </div>
